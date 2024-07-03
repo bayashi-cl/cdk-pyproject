efded75f678f8ac383f4ff2a0f57494013295e13
Masaki Kobayashi <bayashi.cl@gmail.com>
2024-07-04 06:22:07 +0900

    update noxfile

--- noxfile.py
+++ noxfile.py
@@ -1,40 +1,43 @@
+import json
 import os
+import subprocess
 from pathlib import Path
+from typing import TypedDict
 
 import nox
 
 PYTHON_VERSIONS = ["3.11", "3.12"]
-VENV_BACKEND = "uv"
+nox.options.default_venv_backend = "uv"
 
 
-def ensurepath() -> None:
-    rye_home = os.getenv("RYE_HOME")
-    rye_py = Path(rye_home) if rye_home else Path.home() / ".rye" / "py"
-
-    for py_dir in rye_py.iterdir():
-        bin_dir = py_dir / "bin"
-        os.environ["PATH"] = f"{bin_dir}:{os.environ['PATH']}"
+class ToolchainInfo(TypedDict):
+    name: str
+    path: str
 
 
-ensurepath()
+def ensure_python() -> None:
+    for version in PYTHON_VERSIONS:
+        subprocess.check_call(["rye", "toolchain", "fetch", version])  # noqa:  S603, S607
+
+    output = subprocess.check_output(["rye", "toolchain", "list", "--format", "json"], text=True)  # noqa:  S603, S607
+    toolchain_list: list[ToolchainInfo] = json.loads(output)
+    paths = env_path.split(":") if (env_path := os.getenv("PATH")) is not None else []
+    python_paths = [str(Path(toolchain["path"]).parent) for toolchain in toolchain_list]
+
+    os.environ["PATH"] = ":".join(python_paths + paths)
 
 
-@nox.session()
-def fetch(session: nox.Session) -> None:
-    for require_version in PYTHON_VERSIONS:
-        session.run("rye", "fetch", require_version, external=True)
-
-    ensurepath()
+ensure_python()
 
 
-@nox.session(python=PYTHON_VERSIONS, venv_backend=VENV_BACKEND)
+@nox.session(python=PYTHON_VERSIONS)
 def tests(session: nox.Session) -> None:
     session.install("-r", "requirements-dev.lock")
     session.run("pytest")
     session.run("mypy", ".")
 
 
-@nox.session(venv_backend=VENV_BACKEND)
+@nox.session()
 def lint(session: nox.Session) -> None:
     session.install("-r", "requirements-dev.lock")
     session.run("ruff", "check")
